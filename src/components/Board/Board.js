ed4e094e3880143f7bad5d43ab5c0499bb5fe44b
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-06-24 20:18:47 -0400

    Game logic completed.

---
+++ src/components/Board/Board.js
@@ -0,0 +1,20 @@
+import Square from "../Square/Square";
+
+
+const Board = ({ squares, onClick }) => {
+
+    return (
+        <div className="board">
+            {squares.map((square, i) => (
+                <Square
+                    key={i}
+                    value={square}
+                    onClick={() => onClick(i)}
+                />
+            ))}
+        </div>
+    );
+};
+
+
+export default Board;
